275fd2b187a339e631f1f232ba2f300328448889
JonneFelix <Jonne.F.Schwegmann@stud.leuphana.de>
2024-05-08 07:46:06 +0200

    Webhook und backend python getrennt und webhook wieder jetzt ignoriert - 08.05.2024

--- Backend.py
+++ Backend.py
@@ -1,40 +1,17 @@
-from flask import Flask, request, render_template, abort
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import subprocess
-import hmac
-import hashlib
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-# Geheimnis f�r GitHub Webhook
-GITHUB_SECRET = b"mnjrmfkensnfjrvhr" #Diesen secret habe ich in Github repo erstellt
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# WebSocket-Event-Handler f�r den Chat
 @socketio.on('message')
 def handle_message(msg):
-    # Echo-Nachricht zur�cksenden
-    emit('message', msg)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    signature = request.headers.get('X-Hub-Signature-256')
-    if signature is None:
-        abort(400)
-
-    sha_name, signature = signature.split('=')
-    mac = hmac.new(GITHUB_SECRET, msg=request.data, digestmod=hashlib.sha256)
-
-    if not hmac.compare_digest(mac.hexdigest(), signature):
-        abort(400)
-
-    # Git-Pull ausf�hren
-    subprocess.Popen(['git', '-C', '/home/jonne/Programme/KIAgentenPyCharming', 'pull'])
-    return '', 204
+    # Echos die erhaltene Nachricht zur�ck an den Absender
+    emit('message', f'Echo: {msg}', broadcast=False)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
